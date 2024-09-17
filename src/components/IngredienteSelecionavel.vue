6673e917184c5f771e65896669f8313c21a74786
Igor Motta <igor_mota@outlook.com>
2024-09-17 10:18:54 -0300

    second commit

---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import Tag from './Tag.vue';
+  export default {
+    components: { Tag },
+    props: {
+      ingrediente: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        selecionado: false
+      }
+    },
+    methods: {
+      aoClicar() {
+        this.selecionado = !this.selecionado;
+
+        if (this.selecionado) {
+          this.$emit('adicionar', this.ingrediente);
+        } else {
+          this.$emit('remover', this.ingrediente);
+        }
+      }
+    },
+    emits: ['adicionar', 'remover']
+  }
+</script>
+<template>
+  <button
+    class="ingrediente"
+    @click="aoClicar"
+    :aria-pressed="selecionado"
+  >
+    <Tag :texto="ingrediente" :ativa="selecionado" />
+  </button>
+</template>
+
+<style scoped>
+.ingrediente {
+  cursor: pointer;
+}
+</style>
